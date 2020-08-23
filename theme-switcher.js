7410484ff13a4b8f4d8734d704e138c1175119dd
Flippette <quangdatle2006@outlook.com>
2020-08-23 09:24:40 +0700

    Initial UI concept
    Added a navbar and a header. That's it.

---
+++ theme-switcher.js
@@ -0,0 +1,32 @@
+var themeSwitchButton = document.getElementById("theme-switcher");
+
+link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "./dark-core.css";
+link.id = "dark-core"
+
+function parseItem(item) {
+    return typeof item == "string" ? JSON.stringify(item) : item;
+}
+
+function changeTheme(useDarkTheme) {
+    if (useDarkTheme) {
+        document.getElementsByTagName("head")[0].appendChild(link);
+    } else {
+        document.getElementsByTagName("head")[0].removeChild(link);
+    }
+}
+
+document.onload = function() {
+    changeTheme(JSON.parse(localStorage.getItem("useDarkCore")));
+}
+
+themeSwitchButton.onclick = function() {
+    var darkCoreStyles = document.querySelectorAll("head #dark-core");
+    if (Object.keys(darkCoreStyles).length == 0) {
+        localStorage.setItem("useDarkCore", parseItem(true));
+    } else {
+        localStorage.setItem("useDarkCore", parseItem(false));
+    }
+    changeTheme(JSON.parse(localStorage.getItem("useDarkCore")));
+}
