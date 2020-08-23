3e5b437b5c5ac079bb5a8f9b7478a5f829323981
Flippette <quangdatle2006@outlook.com>
2020-08-24 00:11:04 +0700

    All pages are designed (except for SauceDB)
    Read the title. And also added persistent storage for themes. And a beta disclaimer.

--- theme-switcher.js
+++ theme-switcher.js
@@ -17,16 +17,21 @@ function changeTheme(useDarkTheme) {
     }
 }
 
-document.onload = function() {
+document.body.onload = function () {
     changeTheme(JSON.parse(localStorage.getItem("useDarkCore")));
 }
 
 themeSwitchButton.onclick = function() {
     var darkCoreStyles = document.querySelectorAll("head #dark-core");
-    if (Object.keys(darkCoreStyles).length == 0) {
-        localStorage.setItem("useDarkCore", parseItem(true));
-    } else {
-        localStorage.setItem("useDarkCore", parseItem(false));
-    }
-    changeTheme(JSON.parse(localStorage.getItem("useDarkCore")));
+    // if (Object.keys(darkCoreStyles).length == 0) {
+    //     localStorage.setItem("useDarkCore", parseItem(true));
+    // } else {
+    //     localStorage.setItem("useDarkCore", parseItem(false));
+    // }
+    // changeTheme(JSON.parse(localStorage.getItem("useDarkCore")));
+
+    var useDarkCore = Object.keys(darkCoreStyles).length == 0;
+
+    changeTheme(useDarkCore);
+    localStorage.setItem("useDarkCore", parseItem(useDarkCore));
 }
